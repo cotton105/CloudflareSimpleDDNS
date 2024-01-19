4e07ecd330eb1104b066c2b7848d985a89c1d4ca
Luke Cotton <luke@cotton.org.uk>
2024-01-19 09:33:27 +0000

    Compare cached to current IP address

--- update.sh
+++ update.sh
@@ -1,2 +1,22 @@
+#!/bin/bash
+
+##########################################################################
+# This is intended to be set up as a cron job.
+# It will periodically check whether the public IP has changed, and update
+# Cloudflare accordingly.
+##########################################################################
+
+cache_file='lastpinged.txt'
+
 current_ip=$(curl -s ifconfig.co)
-echo $current_ip
+cached_ip="$(cat $cache_file)"
+
+if [ $current_ip != $cached_ip ]; then
+    echo run api command!
+else
+    echo "don't run api command!"
+fi
+
+echo ifconfig: $current_ip
+echo cached: $cached_ip
+sleep 1s
