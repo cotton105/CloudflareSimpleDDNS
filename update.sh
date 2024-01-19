046cdab3d070fb1c1dc1e719bfa4bf20efe6c4d4
Luke Cotton <luke@cotton.org.uk>
2024-01-19 10:40:39 +0000

    Add cURL request to Cloudflare

--- update.sh
+++ update.sh
@@ -12,10 +12,12 @@ API_EMAIL=
 API_KEY=
 API_ZONE_ID=
 API_ACCOUNT_ID=
+DOMAIN_NAME=
+PROXIED=
 EOF
 fi
 source $api_config_file
-if [ -z $API_EMAIL -o -z $API_KEY ]; then
+if [ -z $API_EMAIL ] || [ -z $API_KEY ] || [ -z $API_ZONE_ID ] || [ -z API_ACCOUNT_ID ] || [ -z DOMAIN_NAME ] || [ -z PROXIED ]; then
     echo API details must be specified in api.conf.
     exit 1
 fi
@@ -30,6 +32,19 @@ cached_ip="$(cat $cache_file)"
 
 if [ $current_ip != $cached_ip ]; then
     echo run api command!
+    curl --request PUT \
+        --url "https://api.cloudflare.com/client/v4/zones/$API_ZONE_ID/dns_records/$API_ACCOUNT_ID" \
+        --header "Content-Type: application/json" \
+        --header "X-Auth-Email: $API_EMAIL" \
+        --header "X-Auth-Key: $API_KEY" \
+        --data "{
+            'content': '$current_ip',
+            'name': '$DOMAIN_NAME',
+            'proxied': $PROXIED,
+            'type': 'A',
+            'comment': 'Updated by AutoDNSUpdate on $(date)',
+            'ttl': 1
+        }"
     echo $current_ip > $cache_file
 else
     echo "don't run api command!"
